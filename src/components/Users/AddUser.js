931b8d8b525d82c324e402c20ff49e54f5dc509b
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-05-30 22:32:47 +0530

    Added User Input State

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,25 +1,33 @@
-import React from 'react'
-import Card from '../UI/Card';
-import Button from '../UI/Button';
-import styles from './AddUser.module.css';
-
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import styles from "./AddUser.module.css";
 
 const AddUser = () => {
-    
-    
-    const addUserHandler = (event) => {
-        event.preventDefault();
-    }
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
 
-    return (
-        <Card onSubmit={addUserHandler} className={styles.input}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
-            <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number" />
-            <Button type="submit">Add User</Button>
-        </Card>
-    )
-}
+  const usernameHandler = (event) =>{
+      setEnteredUsername(event.target.value);
+  }
 
-export default AddUser
+  const ageHandler = (event) =>{
+      setEnteredAge(event.target.value);
+  }
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card onSubmit={addUserHandler} className={styles.input}>
+      <label htmlFor="username">Username</label>
+      <input id="username" value={enteredUsername} type="text" onChange={usernameHandler} />
+      <label htmlFor="age">Age (Years)</label>
+      <input id="age" type="number" value={enteredAge} onChange={ageHandler} />
+      <Button type="submit">Add User</Button>
+    </Card>
+  );
+};
+
+export default AddUser;
