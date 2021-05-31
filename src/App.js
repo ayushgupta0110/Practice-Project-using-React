a8188e7610b6501b8694acf68721faef7c0adbd6
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-05-31 22:14:49 +0530

    Managing List of Users using lifting State concept

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React,{ useState} from 'react';
 import AddUser from './components/Users/AddUser';
 import UserList from './components/Users/UserList';
 
 function App() {
+  const [users,setUsers] = useState([]);
+
+  const addUserHandler = (username,age) => {
+      setUsers((prevUsers) => {
+        return [...prevUsers,{username: username,age: age,id: Math.random().toString()}];
+      });
+  }
+
   return (
     <div>
-      <AddUser />
-      <UserList />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={users}/>
     </div>
   );
 }
--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import styles from './UserList.module.css';
+import Card from '../UI/Card';
 
 const UserList = (props) => {
     return (
-        <div className={styles.users}>
+        <Card className={styles.users}>
             <ul>
                 {props.users.map((user) => {
-                   return <li>{user.username} ({user.age} years)</li>
+                   return <li key={user.id}>{user.username} ({user.age} years)</li>
                 })}
             </ul>
-        </div>
+        </Card>
     )
 }
 
